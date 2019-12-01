7c5f12825265da0534092549c6987640af0141e0
Trương Trung Tín <46252881+trungtin230599@users.noreply.github.com>
2019-12-02 00:39:32 +0700

    submit

--- database.php
+++ database.php
@@ -1,3 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</head>
+<body>
 <?php
 
 $servername = "us-cdbr-iron-east-05.cleardb.net";
@@ -55,3 +68,9 @@ function test_input($data) {
     $data = htmlspecialchars($data);
     return $data;
 }
+
+?>
+    <a class="btn btn-default" href="index.php">Back</a>
+</body>
+
+

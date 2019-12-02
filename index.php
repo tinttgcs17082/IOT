afd0e227c755e37a638c179aebc3a3271a7462cd
Trương Trung Tín <46252881+trungtin230599@users.noreply.github.com>
2019-12-03 00:55:27 +0700

    version 2

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="vi">
 
 <head>
-    <title>Bootstrap Example</title>
+    <title>INTERNET OF THINGS</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
--- mqtt.php
+++ mqtt.php
@@ -1,18 +1,45 @@
 <?php
 
 require("./phpMQTT.php");
-$server = "tailor.cloudmqtt.com";     
-$port = 15313;                    
-$username = "jwlfxwbw";                
-$password = "xaoizh2liQ2y";                
+$server = "tailor.cloudmqtt.com";
+$port = 15313;
+$username = "jwlfxwbw";
+$password = "xaoizh2liQ2y";
 $client_id = "phpMQTT-publisher";
 $mqtt = new phpMQTT($server, $port, $client_id);
 if ($mqtt->connect(true, NULL, $username, $password)) {
-	$mqtt->publish("home/garden/fountain", "".$_REQUEST['text'] );
+    $mqtt->publish("home/garden/fountain", "" . $_REQUEST['text']);
     $mqtt->close();
-    header('Location: index.php?text='.$_REQUEST['text'].'');
+    $servername = "us-cdbr-iron-east-05.cleardb.net";
+    $dbname = "heroku_4c775eb85947f23";
+    $username = "b660d87a23e80f";
+    $password = "6a65c1a0";
+
+    $text = "";
+    $text = test_input($_REQUEST["text"]);
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO `text` (`Text`) VALUES ('" . $text . "')";
+
+    if ($conn->query($sql) === TRUE) { } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+    header('Location: index.php?text=' . $_REQUEST['text'] . '');
 } else {
     echo "Time out!\n";
 }
 
-?>
+
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}

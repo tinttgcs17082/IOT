622ebe1123322900001d171b87157be2badfa194
truongtrungtin <trungtin.230599@hotmail.com>
2019-12-22 09:42:56 +0700

    fix

--- index.php
+++ index.php
@@ -122,8 +122,8 @@
                 }       
             }
         }
-        $new_array_time = array_slice($array_Time, -1, -5);
-        $new_array_humi = array_slice($array_Humi, -1, -5); 
+        $new_array_time = array_reverse(array_slice($array_Time, 10, 0), True);
+        $new_array_humi = array_reverse(array_slice($array_Humi, 10, 0), True); 
     ?>
     <script>
 // line chart data
